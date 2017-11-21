92631eb3bc12b23fee0d706e5bdbea87d031d10f
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-21 16:45:19 +0100

    Sprache einstellen und DatePipe

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  d = new Date();
   title = 'Book Rating';
   url = 'https://angular.schule';
 
--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import localeDe from '@angular/common/locales/de';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
@@ -20,7 +21,13 @@ import { ButtonComponent } from './button/button.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de' }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(localeDe);
+  }
+}
