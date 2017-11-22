cb9fd8de9f97cf65a54fd0eded36a7815f402cb4
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-22 15:14:01 +0100

    Tests für Datum

--- book-rating/src/app/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/dashboard/dashboard.component.spec.ts
@@ -1,7 +1,12 @@
 import { By } from '@angular/platform-browser';
-import { Book } from '../shared/book';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+import { DatePipe } from '@angular/common';
+
+import { Book } from '../shared/book';
 import { BookComponent } from '../book/book.component';
 import { DashboardComponent } from './dashboard.component';
 
@@ -11,9 +16,14 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent, BookComponent ]
+      declarations: [ DashboardComponent, BookComponent ],
+      providers: [
+        { provide: LOCALE_ID, useValue: 'de' }
+      ]
     })
     .compileComponents();
+
+    registerLocaleData(localeDe);
   }));
 
   beforeEach(() => {
@@ -41,5 +51,18 @@ describe('DashboardComponent', () => {
 
     // prüfen, wie viele Elemente vorhanden sind
     expect(comps.length).toEqual(component.books.length);
+  });
+
+  it('should display the date', () => {
+    const dateBox = fixture.debugElement.query(By.css('span.date-box'));
+
+    // mit DatePipe
+    const valueFromPipe = new DatePipe('de').transform(component.d, 'fullDate');
+    expect(dateBox.nativeElement.textContent).toContain(valueFromPipe);
+
+    // mit fest definiertem Datum
+    component.d = new Date('2017-03-22 12:34:56');
+    fixture.detectChanges();
+    expect(dateBox.nativeElement.textContent).toContain('Mittwoch, 22. März 2017');
   });
 });
