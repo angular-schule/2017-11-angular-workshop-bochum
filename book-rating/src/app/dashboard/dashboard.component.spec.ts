f2ca418139ea2d364a70c60611d2492069fcb766
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-22 14:41:30 +0100

    Integrationstest Dashboard mit BookComponent

--- book-rating/src/app/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,8 @@
+import { By } from '@angular/platform-browser';
+import { Book } from '../shared/book';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BookComponent } from '../book/book.component';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -8,7 +11,7 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent, BookComponent ]
     })
     .compileComponents();
   }));
@@ -21,5 +24,22 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display a BookComponent for each book', () => {
+    // Books-Array füllen
+    component.books = [
+      new Book('', '', ''),
+      new Book('', '', ''),
+      new Book('', '', '')
+    ];
+
+    fixture.detectChanges();
+
+    // Element(e) holen
+    const comps = fixture.debugElement.queryAll(By.css('br-book'));
+
+    // prüfen, wie viele Elemente vorhanden sind
+    expect(comps.length).toEqual(component.books.length);
   });
 });
