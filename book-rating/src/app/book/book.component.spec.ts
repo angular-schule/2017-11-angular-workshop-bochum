eb55763efbc8f571182105dc3c36bd6131013f20
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-22 14:23:07 +0100

    Shallow Unit Test für BookComponent

--- book-rating/src/app/book/book.component.spec.ts
+++ book-rating/src/app/book/book.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
+import { Book } from '../shared/book';
 import { BookComponent } from './book.component';
 
 describe('BookComponent', () => {
@@ -16,10 +18,35 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = new Book('isbn', 'title', 'description');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('clicking button should invoke rateUp() method', () => {
+    // Button selektieren
+    const btn = fixture.debugElement.query(By.css('button.rate-up-btn'));
+
+    spyOn(component, 'rateUp');
+
+    // Button klicken
+    btn.nativeElement.click();
+
+    // wurde Methode aufgerufen?
+    expect(component.rateUp).toHaveBeenCalled();
+  });
+
+
+  it('should display the book title', () => {
+    const cardTitle = fixture.debugElement.query(By.css('div.card-title'));
+
+    expect(cardTitle.nativeElement.textContent)
+      .toContain(component.book.title);
+
+    component.book.title = 'Anderer Titel';
+
+    fixture.detectChanges();
+
+    expect(cardTitle.nativeElement.textContent)
+      .toContain('Anderer Titel');
   });
 });
