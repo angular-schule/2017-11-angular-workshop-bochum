27dff48c766868ba1fe21f52385ede84ee10a6d4
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-11-23 10:50:31 +0100

    TDF: resetForm

--- book-rating/src/app/create-book-tdf/create-book-tdf.component.ts
+++ book-rating/src/app/create-book-tdf/create-book-tdf.component.ts
@@ -1,12 +1,16 @@
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Book } from '../shared/book';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'br-create-book-tdf',
   templateUrl: './create-book-tdf.component.html',
   styleUrls: ['./create-book-tdf.component.scss']
 })
-export class CreateBookTdfComponent implements OnInit {
+export class CreateBookTdfComponent implements OnInit, AfterViewInit {
+  // @ViewChild('form') bookForm: NgForm;
+  @ViewChild(NgForm) bookForm: NgForm;
 
   book = Book.empty();
 
@@ -14,11 +18,16 @@ export class CreateBookTdfComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    // erst hier ist ViewChild aufgelöst
   }
 
   add() {
     this.bookCreated.emit(this.book);
+    this.book = Book.empty();
+    this.bookForm.resetForm();
   }
 
 }
